4321d3743c0da85f7b002391ee1ff4fa9f3a7717
u111260 <nicolofabio.bordin01@estudiant.upf.edu>
2014-12-25 18:04:49 +0100

    Tutorial1 Italian
    I added the first tutorial in Italian.

--- Tutorials/js/index.js
+++ Tutorials/js/index.js
@@ -3,17 +3,24 @@ window.onload = function () {
   // Get the buttons.
   var styledModalBtn = document.getElementById('launchStyledModal');
   var espanolBtn = document.getElementById('launchEspanol');
+  var italianoBtn = document.getElementById('launchItaliano');
   var closeBtns = document.querySelectorAll('.close');
-  
   
   // Get the dialogs.
   var styledModal = document.getElementById('styledModal');
   var espanol = document.getElementById('espanol');
-
+  var italiano = document.getElementById('italiano');
+  
   styledModalBtn.addEventListener('click', function(e) {
     e.preventDefault();
     
     styledModal.showModal();
+  });
+  
+  italianoBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    
+    italiano.showModal();
   });
   
   espanolBtn.addEventListener('click', function(e) {
